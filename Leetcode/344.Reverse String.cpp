c3a27ae20b7bf04c07717a3b37cc73e3ffac5c50
girish004 <65026577+girish004@users.noreply.github.com>
2020-10-05 11:03:56 +0530

    Add files via upload

---
+++ Leetcode/344.Reverse String.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int j=s.size();
+        int i=-1;
+        while(++i<--j)
+        {
+            swap(s[i],s[j]);
+        }
+    }
+};
